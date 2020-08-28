87da8580ac384a035b60ae7c3b8e7a76f540bb25
ManoVardasNedas <48032428+ManoVardasNedas@users.noreply.github.com>
2020-08-28 18:05:18 +0300

    Added Service folder, construct function
    service folder has php files with extra methods.

--- app/Http/Controllers/reservationInfoController.php
+++ app/Http/Controllers/reservationInfoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\reservation_service;
 use App\specialist;
 use Illuminate\Http\Request;
 
 class reservationInfoController extends Controller
 {
+
+    private $serviceReservation;
+
+    public function __construct()
+    {
+        $this->serviceReservation = new reservation_service();
+    }
+
     public function showReservationInfo(Request $request)
     {
+        $code = $request->input('res_code');
+        
         return view('reservation_info');
     }
 }
