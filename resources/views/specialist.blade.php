05c1a0e3473ec07ce0e032ea4f7ecf85ed9ccb09
ManoVardasNedas <48032428+ManoVardasNedas@users.noreply.github.com>
2020-08-28 23:25:43 +0300

    Update specialist.blade.php
    specialist can cancel reservation

--- resources/views/specialist.blade.php
+++ resources/views/specialist.blade.php
@@ -7,7 +7,14 @@
             Reservation code: {{$reservation -> code}} <br>
             Reservation time: {{$reservation -> dateTime}} <br>
             Reservation status: {{$reservation -> status}} <br>
-            
+
+            <form method="POST" action="/reservation/cancel">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="res_code" value="{{$reservation -> code}}">
+                <button type="submit" >
+                    Cancel Reservation
+                </button>
+            </form>
         </p>
     @endforeach
 </div>
